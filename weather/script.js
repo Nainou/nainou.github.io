3eb8f522352d128db52af4e3cc51879741e0af05
Nainou <55667988+Nainou@users.noreply.github.com>
2023-05-05 21:21:22 +0300

    header+animation

--- weather/script.js
+++ weather/script.js
@@ -1,4 +1,5 @@
 const tableBody = document.getElementById("weather-table-body");
+const dataTypeHeader = document.getElementById("data-type-header");
 const mean = document.getElementById('mean');
 const median = document.getElementById('median');
 const mode = document.getElementById('mode');
@@ -9,9 +10,49 @@ const chartContainer = document.getElementById('chart-container');
 const canvas = document.getElementById('myChart');
 const valueDropDown = document.getElementById("valueDropDown");
 const timeDropDown = document.getElementById("timeDropDown");
-let measurement;
+let isLatest = false;
 
-async function createTable(date, time, value) {
+async function createTable() {
+  const table = document.getElementById("weather-table");
+  let thead = table.querySelector("thead");
+
+  // Fade out existing header row if it exists
+  if (thead) {
+      table.removeChild(thead);
+  }
+
+  // Create new header row
+  thead = document.createElement("thead");
+  const row = document.createElement("tr");
+
+  const dateHeader = document.createElement("th");
+  dateHeader.innerText = "Date";
+  row.appendChild(dateHeader);
+
+  const timeHeader = document.createElement("th");
+  timeHeader.innerText = "Time";
+  row.appendChild(timeHeader);
+
+  const valueHeader = document.createElement("th");
+  valueHeader.innerText = "Value";
+  row.appendChild(valueHeader);
+
+  if (isLatest) {
+    const dataTypeHeader = document.createElement("th");
+    dataTypeHeader.innerText = "Data Type";
+    row.appendChild(dataTypeHeader);
+  }
+
+  thead.appendChild(row);
+  table.appendChild(thead);
+
+  // Fade in new header row
+  fadeIn(thead, 200);
+}
+
+async function addDataToTable(date, time, value, dataType) {
+  const tableBody = document.getElementById("weather-table-body");
+
   const row = document.createElement("tr");
 
   const dateCell = document.createElement("td");
@@ -27,10 +68,23 @@ async function createTable(date, time, value) {
   valueCell.innerText = formattedValue;
   row.appendChild(valueCell);
 
+  if (isLatest) {
+    const dataTypeCell = document.createElement("td");
+    // Capitalize first letter of data type
+    dataType = dataType.charAt(0).toUpperCase() + dataType.slice(1);
+    //turn _ to space
+    dataType = dataType.replace(/_/g, ' ');
+    dataTypeCell.innerText = dataType;
+    row.appendChild(dataTypeCell);
+  }
+  // Fade in the new row
   tableBody.appendChild(row);
+  fadeIn(tableBody, 500);
 }
 
 async function fetchLastValues() {
+  isLatest = true;
+  await createTable();
   fetch('https://webapi19sa-1.course.tamk.cloud/v1/weather/limit/50')
     .then(response => response.json())
     .then(data => {
@@ -59,7 +113,7 @@ async function fetchLastValues() {
         }
 
         const value = weatherData[dataType];
-        createTable(date, time, value);
+        addDataToTable(date, time, value, dataType);
       });
       chartContainer.style.display = 'none';
 
@@ -68,22 +122,24 @@ async function fetchLastValues() {
 }
 
 async function fetchTemperature() {
+  isLatest = false;
   fetchValue("temperature");
 }
 
 async function fetchLight() {
+  isLatest = false;
   fetchValue("light");
 
 }
 
 async function fetchValue(dataType) {
-  const valueDropDownValue = valueDropDown.options[valueDropDown.selectedIndex].value;
   const timeDropDownValue = timeDropDown.options[timeDropDown.selectedIndex].value;
 
   let fetchURL = `https://webapi19sa-1.course.tamk.cloud/v1/weather/${dataType}`;
   if (timeDropDownValue !== 'Now') {
     fetchURL += `/${timeDropDownValue}`;
   }
+  createTable();
   fetch(fetchURL)
     .then(response => response.json())
     .then(data => {
@@ -94,7 +150,7 @@ async function fetchValue(dataType) {
         const date = dateTime.toLocaleDateString();
         const time = dateTime.toLocaleTimeString();
         const value = weather[dataType];
-        createTable(date, time, value);
+        addDataToTable(date, time, value);
       });
       createChart(data, dataType);
       calculations(data, dataType);
@@ -171,85 +227,126 @@ async function fetchDropdown() {
 
 async function calculations(data, dataType) {
   //fade calculationsbox out
-  fadeOut();
+  await fadeOut(calculationsDiv, 200)
 
-  //add a delay to do calculations after fadeout
-  setTimeout(() => {
+  // mean
+  const allValues = data.map(function (value) {
+    return parseFloat(value[dataType]);
+  });
 
-    // mean
-    const allValues = data.map(function (value) {
-      return parseFloat(value[dataType]);
-    });
+  let count = 0;
+  let sum = 0;
+  for (let i = 0; i < allValues.length; i++) {
+    if (!isNaN(allValues[i])) {
+      count = count + 1;
+      sum = sum + allValues[i];
+    }
+  }
+  let meanValue = sum / count;
+  mean.innerHTML = "Mean: " + meanValue.toFixed(2);
 
+  // median
+  let sortedValues = allValues.sort(function (a, b) {
+    return a - b;
+  });
+  let medianValue = 0;
+  if (count % 2 === 0) {
+    let half = count / 2;
+    medianValue = (sortedValues[half - 1] + sortedValues[half]) / 2;
+  } else {
+    let half = Math.floor(count / 2);
+    medianValue = sortedValues[half];
+  }
+  median.innerHTML = "Median: " + medianValue.toFixed(2);
+
+  // mode
+  let modeValue = 0;
+  let maxCount = 0;
+  for (let i = 0; i < allValues.length; i++) {
     let count = 0;
-    let sum = 0;
-    for (let i = 0; i < allValues.length; i++) {
-      if (!isNaN(allValues[i])) {
-        count = count + 1;
-        sum = sum + allValues[i];
+    for (let j = 0; j < allValues.length; j++) {
+      if (allValues[j] === allValues[i]) {
+        count++;
       }
     }
-    let meanValue = sum / count;
-    mean.innerHTML = "Mean: " + meanValue.toFixed(2);
-
-    // median
-    let sortedValues = allValues.sort(function (a, b) {
-      return a - b;
-    });
-    let medianValue = 0;
-    if (count % 2 === 0) {
-      let half = count / 2;
-      medianValue = (sortedValues[half - 1] + sortedValues[half]) / 2;
-    } else {
-      let half = Math.floor(count / 2);
-      medianValue = sortedValues[half];
+    if (count > maxCount) {
+      modeValue = allValues[i];
+      maxCount = count;
     }
-    median.innerHTML = "Median: " + medianValue.toFixed(2);
+  }
+  mode.innerHTML = "Mode: " + modeValue.toFixed(2);
 
-    // mode
-    let modeValue = 0;
-    let maxCount = 0;
-    for (let i = 0; i < allValues.length; i++) {
-      let count = 0;
-      for (let j = 0; j < allValues.length; j++) {
-        if (allValues[j] === allValues[i]) {
-          count++;
-        }
-      }
-      if (count > maxCount) {
-        modeValue = allValues[i];
-        maxCount = count;
-      }
-    }
-    mode.innerHTML = "Mode: " + modeValue.toFixed(2);
+  // range
+  let sortedAllValues = allValues.sort(function (a, b) {
+    return a - b;
+  });
+  let minValue = sortedAllValues[0];
+  let maxValue = sortedAllValues[sortedAllValues.length - 1];
+  let rangeValue = maxValue - minValue;
+  range.innerHTML = "Range: " + rangeValue.toFixed(2);
 
-    // range
-    let sortedAllValues = allValues.sort(function (a, b) {
-      return a - b;
-    });
-    let minValue = sortedAllValues[0];
-    let maxValue = sortedAllValues[sortedAllValues.length - 1];
-    let rangeValue = maxValue - minValue;
-    range.innerHTML = "Range: " + rangeValue.toFixed(2);
+  // standard deviation
+  let deviationSum = 0;
+  for (let i = 0; i < allValues.length; i++) {
+    deviationSum = deviationSum + Math.pow((allValues[i] - meanValue), 2);
+  }
+  let variance = deviationSum / count;
+  let standardDeviationValue = Math.sqrt(variance);
+  standardDeviation.innerHTML = "Standard Deviation: " + standardDeviationValue.toFixed(2);
 
-    // standard deviation
-    let deviationSum = 0;
-    for (let i = 0; i < allValues.length; i++) {
-      deviationSum = deviationSum + Math.pow((allValues[i] - meanValue), 2);
-    }
-    let variance = deviationSum / count;
-    let standardDeviationValue = Math.sqrt(variance);
-    standardDeviation.innerHTML = "Standard Deviation: " + standardDeviationValue.toFixed(2);
-
-    // fade calculationsbox in
-    fadeIn();
-  }, 300);
+  // fade calculationsbox in
+  await fadeIn(calculationsDiv, 200)
 }
 
-function fadeIn() {
-  calculationsDiv.classList.add('show');
+
+// Fade out function
+function fadeOut(element, duration) {
+  return new Promise(resolve => {
+    element.style.opacity = 1;
+
+    let start = null;
+
+    function step(timestamp) {
+      if (!start) {
+        start = timestamp;
+      }
+
+      const progress = timestamp - start;
+      element.style.opacity = 1 - Math.min(progress / duration, 1);
+
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      } else {
+        resolve();
+      }
+    }
+
+    window.requestAnimationFrame(step);
+  });
 }
 
-function fadeOut() {
-  calculationsDiv.classList.remove('show');
+// Fade in function
+function fadeIn(element, duration) {
+  return new Promise(resolve => {
+    element.style.opacity = 0;
+
+    let start = null;
+
+    function step(timestamp) {
+      if (!start) {
+        start = timestamp;
+      }
+
+      const progress = timestamp - start;
+      element.style.opacity = Math.min(progress / duration, 1);
+
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      } else {
+        resolve();
+      }
+    }
+
+    window.requestAnimationFrame(step);
+  });
 }
