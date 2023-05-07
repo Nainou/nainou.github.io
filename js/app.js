fe55d39a2700be6474b7ff5483f915e03ad38dfa
Nainou <55667988+Nainou@users.noreply.github.com>
2023-05-07 17:44:13 +0300

    final

--- js/app.js
+++ js/app.js
@@ -69,7 +69,7 @@ particlesJS('particles-js',
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
