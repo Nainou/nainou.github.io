b564b7d9108e14c9fc6ab0bd4ef3dd4c4f1e7782
nainou <mr.tino.ketola@gmail.com>
2024-05-02 17:18:40 +0300

    site

--- cube/cube.js
+++ cube/cube.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('canvas');
+ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const w = canvas.width = 1280;
@@ -202,4 +202,3 @@ document.addEventListener('wheel', (event) => {
 document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
   });
-
